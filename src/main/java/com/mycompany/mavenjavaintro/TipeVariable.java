6630b5517acfc93b73ef91b35b955a62c074566f
Styvensoft <steveen.echeverri@gmail.com>
2022-12-17 21:31:04 -0500

    First commit setup

---
+++ src/main/java/com/mycompany/mavenjavaintro/TipeVariable.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenjavaintro;
+
+/**
+ *
+ * @author s7eveen
+ */
+public class TipeVariable {
+    
+    public void MainVariable() {
+        int age = 26;
+        double salario = 12256.36;
+    
+        System.out.println("Edad: " + age);
+        System.out.println("Salario: " + salario);
+    }
+    
+}
---
+++ src/main/java/com/mycompany/mavenjavaintro/characters.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenjavaintro;
+
+/**
+ *
+ * @author s7eveen
+ */
+public class characters {
+    
+    public void MainChar() {
+        char character = 'a';
+        System.out.println(character);
+        
+        character = 65 + 2;
+        
+        char secondChar = (char) (character + 3);
+        
+        System.out.println(secondChar);
+        
+        
+    }
+}
